62c4af27570450a9782830202963385b0a9275ee
netcam22 <annettelesage@btinternet.com>
2023-10-18 14:27:24 +0100

    feat: implement add funtion with rest parameters

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,9 +1,9 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b) {
-  // Add a loop here
-  return a + b;
+export function add(...nums) {
+  const result = nums.reduce((total, num) => total + num, 0);
+  return result;
 }
 
 add(1, 2, 3, 4, 5);
