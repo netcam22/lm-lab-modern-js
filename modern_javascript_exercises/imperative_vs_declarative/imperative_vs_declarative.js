0fe45271f77710005534baa8387a65fa6af0f1f0
Renata <renatagiera@gmail.com>
2023-10-18 19:59:28 +0100

    feat: complete imperative vs declarative

--- modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
+++ modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
@@ -1,13 +1,8 @@
 // Instructions can be found in imperative_vs_declarative.md
 
 export function longPasswords(passwords) {
-  let longPasswords = [];
-
-  for (let i = 0; i < passwords.length; i++) {
-    const password = passwords[i];
-    if (password.length >= 9) {
-      longPasswords.push(password);
-    }
-  }
+  let longPasswords = passwords.filter((password) => {
+    return password.length >= 9;
+  });
   return longPasswords;
 }
