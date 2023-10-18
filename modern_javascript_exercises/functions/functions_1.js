2fda711a9760034b7469f3d0305a7d67ddf6f164
Renata <renatagiera@gmail.com>
2023-10-18 13:40:51 +0100

    feat: complete functions and variables exercises

--- modern_javascript_exercises/functions/functions_1.js
+++ modern_javascript_exercises/functions/functions_1.js
@@ -23,14 +23,14 @@ const performLogPerson = function logPerson(person) {
 // Arrow Functions in a Variable
 // Write a version of logPerson() as an Arrow Function and store it in a variable called arrowVersionOfLogPerson
 
-const arrowVersionOfLogPerson = person => {
+const arrowVersionOfLogPerson = (person) => {
   console.log("The person is " + person);
 };
 
 // Anonymous Arrow Functions
 // Replace logPerson below with an Anonymous Arrow Function that does the same thing!
 
-people.forEach(person => {
+people.forEach((person) => {
   console.log("The person is " + person);
 });
 
